74c19440af70f05546792fe59fb61991c6733ae5
Giuseppe De Laurentis <g.dl@hotmail.it>
2022-11-04 01:37:38 +0100

    timeit decorator prints only if verbose is requested

--- linac/timeit_decorator.py
+++ linac/timeit_decorator.py
@@ -16,6 +16,7 @@ def timeit(func):
         s, ms = divmod(seconds * 1000, 1000)
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
-        print("\rTime elapsed in %s: %d:%02d:%02d:%03d.                                        " % (func.__name__, h, m, s, ms))
+        if "verbose" in kwargs.keys() and kwargs["verbose"] == True:
+            print("\rTime elapsed in %s: %d:%02d:%02d:%03d.                                        " % (func.__name__, h, m, s, ms))
         return value
     return wrapper_timeit
