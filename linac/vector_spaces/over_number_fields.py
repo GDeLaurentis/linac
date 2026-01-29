4f405c5eb5edb1f177252504ed886ebb8c0f3015
GDeLaurentis <g.dl@hotmail.it>
2026-01-29 20:06:07 +0000

    galois optional interface in vector space of number fields to speed up computation

--- linac/vector_spaces/over_number_fields.py
+++ linac/vector_spaces/over_number_fields.py
@@ -3,6 +3,9 @@
 import fractions
 import numpy
 
+from pyadic.finite_field import vec_ModP
+
+from .._optional import GALOIS_FOUND
 from ..row_reduce import row_reduce
 from ..linear_algebra_tools import drop_bottom_zero_rows, pivot_columns_from_row_reduced_echelon_form, canonical_kernel_from_row_reduced_echelon_form
 
@@ -10,15 +13,32 @@ from ..linear_algebra_tools import drop_bottom_zero_rows, pivot_columns_from_row
 class ColumnVectorSpace(object):
     """Column vector space over number fields (FF and Q supported atm)."""
 
-    def __init__(self, matrix, prime=2147483647):
+    def __init__(self, matrix, prime=2147483647, use_galois=None):
+        if use_galois is None:
+            use_galois = GALOIS_FOUND  # default behaviour
+        self.use_galois = use_galois
+
         if prime is not None:
-            assert prime <= 2 ** 31 - 1
-            self.matrix = matrix % prime
-            self.matrix = self.matrix.astype(numpy.int32)
             self.prime = prime
+            self.matrix = vec_ModP(prime)(matrix)
+
+            if use_galois:
+                if not GALOIS_FOUND:
+                    raise RuntimeError("use_galois=True requested but galois is not installed.")
+                self.matrix = self.matrix.astype(int).view(self.GFp)
+            else:
+                assert prime <= 2**31 - 1
+                self.matrix = self.matrix.astype(numpy.int32)
         else:
             self.matrix = numpy.vectorize(fractions.Fraction, otypes='O')(matrix)
             self.prime = None
+
+    @property
+    def GFp(self):
+        if not self.use_galois:
+            raise ValueError("Tried to access GFp but use_galois is False.")
+        import galois
+        return galois.GF(self.prime)
 
     @property
     def dim(self):
@@ -32,21 +52,25 @@ class ColumnVectorSpace(object):
 
     def __and__(self, other):
         A, B = self.matrix, other.matrix
-        AmB = numpy.block([A, -B]) if self.prime is None else numpy.block([A, -B]) % self.prime
+        AmB = numpy.block([A, -B]) if (self.prime is None or self.use_galois) else numpy.block([A, -B]) % self.prime
         # AmBrref = cuda_row_reduce(AmB, field_characteristic=self.prime)
         AmBrref, _ = row_reduce(AmB, pivoting=1, scaling=False, threshold=0, prime=self.prime)
         AmBrref = drop_bottom_zero_rows(AmBrref)
         AmBkernel = canonical_kernel_from_row_reduced_echelon_form(AmBrref)
-        AmBkernel = AmBkernel if self.prime is None else AmBkernel % self.prime
-        x = AmBkernel[:A.shape[1]].astype(object)  # use object to avoid overflow
-        y = AmBkernel[A.shape[1]:].astype(object)
-        A = A.astype(object)
-        B = B.astype(object)
-        if self.prime is None:
+        AmBkernel = AmBkernel if (self.prime is None or self.use_galois) else AmBkernel % self.prime
+        if self.prime is None or not self.use_galois:
+            x = AmBkernel[:A.shape[1]].astype(object)  # use object to avoid overflow
+            y = AmBkernel[A.shape[1]:].astype(object)
+            A = A.astype(object)
+            B = B.astype(object)
+        else:
+            x = vec_ModP(self.prime)(AmBkernel[:A.shape[1]]).astype(int).view(self.GFp)
+            y = vec_ModP(self.prime)(AmBkernel[A.shape[1]:]).astype(int).view(self.GFp)
+        if self.prime is None or self.use_galois:
             assert numpy.all(A @ x == B @ y)
         else:
             assert numpy.all((A @ x) % self.prime == (B @ y) % self.prime)
-        w = (A @ x) if self.prime is None else (A @ x) % self.prime
+        w = (A @ x) if (self.prime is None or self.use_galois) else (A @ x) % self.prime
         return ColumnVectorSpace(w, self.prime)
 
     def __contains__(self, other):
@@ -63,4 +87,4 @@ class ColumnVectorSpace(object):
         return hash(self) == hash(other)           # might want to revisit this, it is not the intuitive equality
 
     def __hash__(self):
-        return hash(tuple(self.matrix.flatten()))  # this assumes canonical kernel form
+        return hash(tuple(self.matrix.flatten().tolist()))  # this assumes canonical kernel form
--- tests/test_vector_spaces_over_number_fields.py
+++ tests/test_vector_spaces_over_number_fields.py
@@ -1,20 +1,42 @@
 import numpy
+import pytest
 
+from linac import GALOIS_FOUND
+from fractions import Fraction as Q
 from linac import ColumnVectorSpace
 
 
-def test_column_vector_space():
+@pytest.mark.parametrize("use_galois", [False, True])
+def test_column_vector_space(use_galois):
+    if use_galois and not GALOIS_FOUND:
+        pytest.skip("galois not installed")
+
+    def CVS(mat):
+        return ColumnVectorSpace(numpy.array(mat), use_galois=use_galois)
+
+    a = CVS([[1, 1],
+             [1, -1]])
+    b = CVS([[1, 0],
+             [0, 1]])
+    c = CVS([[1],
+             [0]])
+
+    assert a in b and b in a
+    assert (a & c) in c and c in (a & c)
+
+
+def test_column_vector_space_rat():
     a = ColumnVectorSpace(numpy.array([
-        [1, 1],
-        [1, -1]
-    ]))
+        [Q(1), Q(1)],
+        [Q(1), Q(-1)]
+    ], dtype=object), prime=None)
     b = ColumnVectorSpace(numpy.array([
-        [1, 0],
-        [0, 1]
-    ]))
+        [Q(1), Q(0)],
+        [Q(0), Q(1)]
+    ], dtype=object), prime=None)
     c = ColumnVectorSpace(numpy.array([
-        [1, ],
-        [0, ]
-    ]))
+        [Q(1), ],
+        [Q(0), ]
+    ], dtype=object), prime=None)
     assert a in b and b in a
     assert a & c in c and c in a & c
